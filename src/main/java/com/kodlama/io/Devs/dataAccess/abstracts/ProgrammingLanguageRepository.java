282752f40f2b2524ab75c97b49f5ececa40089ed
Burra <gezerbugra@gmail.com>
2022-10-28 23:34:34 +0300

    ProgrammingLanguageRepository created

---
+++ src/main/java/com/kodlama/io/Devs/dataAccess/abstracts/ProgrammingLanguageRepository.java
@@ -0,0 +1,11 @@
+package com.kodlama.io.Devs.dataAccess.abstracts;
+
+import com.kodlama.io.Devs.entities.concretes.ProgrammingLanguage;
+import java.util.ArrayList;
+
+public interface ProgrammingLanguageRepository {
+  ArrayList<ProgrammingLanguage> getAll();
+  ProgrammingLanguage add(ProgrammingLanguage programmingLanguage);
+  ProgrammingLanguage update(ProgrammingLanguage programmingLanguage);
+  void deleteById(int id);
+}
