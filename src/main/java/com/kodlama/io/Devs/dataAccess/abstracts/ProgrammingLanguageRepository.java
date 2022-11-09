158107e89812caf58ebc90b4e92814f77a23b77d
Burra <gezerbugra@gmail.com>
2022-11-10 00:28:27 +0300

    ProgrammingLanguageRepository updated

--- src/main/java/com/kodlama/io/Devs/dataAccess/abstracts/ProgrammingLanguageRepository.java
+++ src/main/java/com/kodlama/io/Devs/dataAccess/abstracts/ProgrammingLanguageRepository.java
@@ -3,10 +3,7 @@ package com.kodlama.io.Devs.dataAccess.abstracts;
 import com.kodlama.io.Devs.entities.concretes.ProgrammingLanguage;
 import java.util.ArrayList;
 
-public interface ProgrammingLanguageRepository {
-  ArrayList<ProgrammingLanguage> getAll();
-  ProgrammingLanguage add(ProgrammingLanguage programmingLanguage);
-  ProgrammingLanguage update(ProgrammingLanguage programmingLanguage);
-  void deleteById(int id);
-  boolean checkIfNameIsAlreadyExists(String name);
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProgrammingLanguageRepository extends JpaRepository<ProgrammingLanguage, Integer> {
 }
