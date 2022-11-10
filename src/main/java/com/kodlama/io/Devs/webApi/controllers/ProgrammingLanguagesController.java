b5e30265bd5a2206603c62ffc2d8d3551af91d4f
Burra <gezerbugra@gmail.com>
2022-11-10 11:30:19 +0300

    ProgrammingLanguagesController updated

--- src/main/java/com/kodlama/io/Devs/webApi/controllers/ProgrammingLanguagesController.java
+++ src/main/java/com/kodlama/io/Devs/webApi/controllers/ProgrammingLanguagesController.java
@@ -1,10 +1,14 @@
 package com.kodlama.io.Devs.webApi.controllers;
 
 import com.kodlama.io.Devs.business.abstracts.ProgrammingLanguageService;
+import com.kodlama.io.Devs.business.requests.CreateProgrammingLanguageRequest;
+import com.kodlama.io.Devs.business.responses.GetAllProgrammingLanguagesResponse;
 import com.kodlama.io.Devs.entities.concretes.ProgrammingLanguage;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +26,13 @@ public class ProgrammingLanguagesController {
   }
 
   @GetMapping("/getall")
-  public List<ProgrammingLanguage> getAll() {
+  public List<GetAllProgrammingLanguagesResponse> getAll() {
     return programmingLanguageService.getAll();
+  }
+  
+  @PostMapping("/add")
+  public void add(@RequestBody() CreateProgrammingLanguageRequest createProgrammingLanguageRequest)
+  {
+	  programmingLanguageService.add(createProgrammingLanguageRequest);
   }
 }
