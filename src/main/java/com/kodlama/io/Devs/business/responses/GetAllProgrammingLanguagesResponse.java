09a4b30bb20242d7e6660de99f312c1a1639d8e5
Burra <gezerbugra@gmail.com>
2022-11-10 11:20:28 +0300

    GetAllProgrammingLanguagesResponse defined

---
+++ src/main/java/com/kodlama/io/Devs/business/responses/GetAllProgrammingLanguagesResponse.java
@@ -0,0 +1,18 @@
+package com.kodlama.io.Devs.business.responses;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetAllProgrammingLanguagesResponse {
+
+	private int id;
+	private String name;
+	private List<String> technologyNames;
+}
+
